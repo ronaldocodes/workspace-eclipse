2ab8b0c465fa8dd5b9ba6054e1b4b041f6f84907
ronaldo <ronaldosec@outlook.com>
2022-11-21 23:30:43 -0300

    006

---
+++ studyExemples/src/estudo/testWhile.java
@@ -0,0 +1,22 @@
+package estudo;
+
+import java.util.Scanner;
+
+public class testWhile {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		
+		int soma = 0;
+		while( x != 0) {
+			soma += x;
+			x = sc.nextInt();
+		}
+		
+		System.out.println(soma);
+		sc.close();
+	}
+
+}
