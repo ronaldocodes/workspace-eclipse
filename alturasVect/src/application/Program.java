d510065723431f6a89a8576aa0e8b1933d53138a
ronaldocodes <ronaldocodes@protonmail.com>
2022-12-14 18:54:33 -0300

    017

--- alturasVect/src/application/Program.java
+++ alturasVect/src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
 		double[] altura = new double[n];
 
 		for (int i = 0; i < n; i++) {
-			System.out.println("Dados da" + (i + 1) + " a pessoa");
+			System.out.println("Dados da " + (i + 1) + " a pessoa");
 			System.out.print("Nome:");
 			nomes[i] = sc.next();
 			System.out.print("Idade:");
